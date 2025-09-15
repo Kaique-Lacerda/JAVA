caa4c7b3b74cbde32c3a60c1a6a0fcc24089e7f5
Kaique Gabriel <149600790+Kaique-Lacerda@users.noreply.github.com>
2025-09-15 19:58:57 +0000

    Remove unused classes and enhance RPG game logic with new enemy encounters

--- RPG.java
+++ RPG.java
@@ -30,7 +30,7 @@ public class RPG{
         System.out.println("2- Arqueiro (vida: 50, Dano: 15 + 3)");
         System.out.println("3- Mago (vida: 70, Dano: 15 + 7)");
         System.out.println("4- Assassino (vida: 60, Dano: 15 + 25)");
-        System.out.println("5- Tanque (vida: 250, Dano: 15 - 5)");
+        System.out.println("5- Tanque (vida: 250, Dano: 15 + 5000)");
         System.out.println("6- Bardo (vida: 10, Dano: 15 - 10)");
 
         int escolha = scanner.nextInt();
@@ -51,7 +51,7 @@ public class RPG{
                 jogador = new Personagem("Assassino", 60, 15 + 25);
                 break;
             case 5:
-                jogador = new Personagem("Tanque", 250, 15 - 5);
+                jogador = new Personagem("Tanque", 250, 15 + 5000);
                 break;
             case 6:
                 jogador = new Personagem("Bardo", 10, 15 - 10);
@@ -59,6 +59,145 @@ public class RPG{
             default: 
                 System.out.println("Escolha inválida");
                 jogador = new Personagem("Bardo", 10, 15 - 10);
+        }
+
+        Personagem inimigo = new Personagem("Goblin", 100, 15 + 1);
+        Personagem inimigo2 = new Personagem("Orc", 150, 15 + 3);
+        Personagem inimigo3 = new Personagem("Troll", 200, 15 + 5);
+        Personagem inimigo4 = new Personagem("Dragão", 300, 15 + 10);
+
+        while (jogador.vida > 0 && inimigo.vida > 0){
+            System.out.println("Sua vida: " + jogador.vida);
+            System.out.println("Vida do inimigo: " + inimigo.vida);
+            System.out.println("Escolha sua ação:");
+            System.out.println("1- Atacar");
+            System.out.println("2- Fugir");
+
+            int acao = scanner.nextInt();
+            scanner.nextLine();
+            if (acao == 1){
+                int danoCausado = jogador.atacar();
+                inimigo.vida -= danoCausado;
+                System.out.println("Você causou " + danoCausado + " de dano ao inimigo.");
+
+                if (inimigo.vida > 0){
+                    int danoRecebido = inimigo.atacar();
+                    jogador.vida -= danoRecebido;
+                    System.out.println("O inimigo causou " + danoRecebido + " de dano a você.");
+                } else {
+                    System.out.println("Você derrotou o inimigo!");
+                }
+            } else if (acao == 2){
+                System.out.println("Você fugiu da batalha.");
+                break;
+            } else {
+                System.out.println("Ação inválida.");
+            }
+        }
+        if (jogador.vida <= 0){
+            System.out.println("Você foi derrotado.");
+        } else if (inimigo.vida <= 0){
+            System.out.println("Você venceu a batalha!");
+            System.out.println("Um novo inimigo aparece: Orc!");
+            inimigo = inimigo2;
+            while (jogador.vida > 0 && inimigo.vida > 0){
+                System.out.println("Sua vida: " + jogador.vida);
+                System.out.println("Vida do inimigo: " + inimigo.vida);
+                System.out.println("Escolha sua ação:");
+                System.out.println("1- Atacar");
+                System.out.println("2- Fugir");
+                int acao = scanner.nextInt();
+                scanner.nextLine();
+                if (acao == 1){
+                    int danoCausado = jogador.atacar();
+                    inimigo.vida -= danoCausado;
+                    System.out.println("Você causou " + danoCausado + " de dano ao inimigo.");  
+                    if (inimigo.vida > 0){
+                        int danoRecebido = inimigo.atacar();
+                        jogador.vida -= danoRecebido;
+                        System.out.println("O inimigo causou " + danoRecebido + " de dano a você.");
+                    } else {
+                        System.out.println("Você derrotou o inimigo!");
+                    }
+                } else if (acao == 2){
+                    System.out.println("Você fugiu da batalha.");
+                    break;
+                } else {
+                    System.out.println("Ação inválida.");
+                }
+            }
+            if (jogador.vida <= 0){
+                System.out.println("Você foi derrotado.");
+            } else if (inimigo.vida <= 0){
+                System.out.println("Você venceu a batalha!");
+                System.out.println("Um novo inimigo aparece: Troll!");
+                inimigo = inimigo3;
+                while (jogador.vida > 0 && inimigo.vida > 0){
+                    System.out.println("Sua vida: " + jogador.vida);
+                    System.out.println("Vida do inimigo: " + inimigo.vida);
+                    System.out.println("Escolha sua ação:");
+                    System.out.println("1- Atacar");
+                    System.out.println("2- Fugir");
+                    int acao = scanner.nextInt();
+                    scanner.nextLine();
+                    if (acao == 1){
+                        int danoCausado = jogador.atacar();
+                        inimigo.vida -= danoCausado;
+                        System.out.println("Você causou " + danoCausado + " de dano ao inimigo.");  
+                        if (inimigo.vida > 0){
+                            int danoRecebido = inimigo.atacar();
+                            jogador.vida -= danoRecebido;
+                            System.out.println("O inimigo causou " + danoRecebido + " de dano a você.");
+                        } else {
+                            System.out.println("Você derrotou o inimigo!");
+                        }
+                    } else if (acao == 2){
+                        System.out.println("Você fugiu da batalha.");
+                        break;
+                    } else {
+                        System.out.println("Ação inválida.");
+                    }
+                }
+                if (jogador.vida <= 0){
+                    System.out.println("Você foi derrotado.");
+                } else if (inimigo.vida <= 0){
+                    System.out.println("Você venceu a batalha!");
+                    System.out.println("Um novo inimigo aparece: Dragão!");
+                    inimigo = inimigo4;
+                    while (jogador.vida > 0 && inimigo.vida > 0){
+                        System.out.println("Sua vida: " + jogador.vida);
+                        System.out.println("Vida do inimigo: " + inimigo.vida);
+                        System.out.println("Escolha sua ação:");
+                        System.out.println("1- Atacar");
+                        System.out.println("2- Fugir");
+                        int acao = scanner.nextInt();
+                        scanner.nextLine();
+                        if (acao == 1){
+                            int danoCausado = jogador.atacar();
+                            inimigo.vida -= danoCausado;
+                            System.out.println("Você causou " + danoCausado + " de dano ao inimigo.");  
+                            if (inimigo.vida > 0){
+                                int danoRecebido = inimigo.atacar();
+                                jogador.vida -= danoRecebido;
+                                System.out.println("O inimigo causou " + danoRecebido + " de dano a você.");
+                            } else {
+                                System.out.println("Você derrotou o inimigo!");
+                            }
+                        }
+                        else if (acao == 2){
+                            System.out.println("Você fugiu da batalha.");
+                            break;
+                        } else {
+                            System.out.println("Ação inválida.");
+                        }
+                    }
+                    if (jogador.vida <= 0){
+                        System.out.println("Você foi derrotado.");
+                    } else if (inimigo.vida <= 0){
+                        System.out.println("Você venceu a batalha final! Parabéns!");
+                    }
+                }
+            }
         }
     }
 }
